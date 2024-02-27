9f8cee8af0628d19f4d721b5883a5e831df94ae0
EJoeyIsCool <159656617+EJoeyIsCool@users.noreply.github.com>
2024-02-26 21:39:48 -0500

    TripleHelixClient.js
    The actual bookmarklet

--- TripleHelixClient.js
+++ TripleHelixClient.js
@@ -1,1 +1,5 @@
+Highlight (triple click) the following code then drag it into your bookmarks bar. 
+Simply change the name of the bookmark to "Triple Helix Client" and you're ready for use on any webpage. (Or at least any webpage that doesn't block JS execution.)
 
+Code:
+javascript:alert("Sorry, this code is still in development, but I'm trying my best to get it out as quickly as possible! Have a great day!";
