f3adab4a485d747a6b8ad3a60f3f40e9e76b916f
EJoeyIsCool <159656617+EJoeyIsCool@users.noreply.github.com>
2024-03-01 22:13:24 -0500

    TripleHelixClient.js

--- TripleHelixClient.js
+++ TripleHelixClient.js
@@ -1,5 +1,6 @@
 Highlight (triple click) the following code then drag it into your bookmarks bar. 
 Simply change the name of the bookmark to "Triple Helix Client" and you're ready for use on any webpage. (Or at least any webpage that doesn't block JS execution.)
 If you run into any errors, an alternative is going to FullCode.js, copying it, and then dragging that into your bookmarks bar. But this is easier.
+
 Code:
 javascript:!function(){var e=document.createElement("script");e.src="https://ejoeyiscool.github.io/Triple-Helix-Client/FullCode.js",e.onerror=function(){alert("Failed to load the script.")},document.body.appendChild(e)}();
